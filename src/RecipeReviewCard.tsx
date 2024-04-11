f93d02ba4f65c9d4020132f0ab25e445ef7e98d0
developer <>
2024-04-11 14:19:26 -0700

    initial

--- src/RecipeReviewCard.tsx
+++ src/RecipeReviewCard.tsx
@@ -14,7 +14,7 @@ import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
-import Receipe from "./assets/images/receipe.jpg"
+import Receipe from "/receipe.jpg"
 import { useTranslation } from 'react-i18next';
 interface ExpandMoreProps extends IconButtonProps {
   expand: boolean;
@@ -57,7 +57,7 @@ export default function RecipeReviewCard() {
       <CardMedia
         component="img"
         height="194"
-        image={Receipe}
+        image={Receipe ?? "/receipe.jpg"}
         alt="Paella dish"
       />
       <CardContent>
